53baf09f9d3cd13508e50ebd49723a608166d1d5
App Team <appteam@appdemacbook.local>
2016-01-15 08:59:06 +0800

    Initial Commit

---
+++ TestTabBarAndNav/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTabBarAndNav
+//
+//  Created by App Team on 16/1/15.
+//  Copyright © 2016年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestTabBarAndNav/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTabBarAndNav
+//
+//  Created by App Team on 16/1/15.
+//  Copyright © 2016年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
